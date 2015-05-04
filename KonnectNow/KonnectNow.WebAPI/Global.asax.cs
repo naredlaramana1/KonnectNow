678cbdc7058f6d533d2e14d2c570b98b596ab3e4
naredlaramana1 <naredlaramana1@gmail.com>
2015-05-05 02:07:05 +0530

    comiited connection string

--- KonnectNow/KonnectNow.WebAPI/Global.asax.cs
+++ KonnectNow/KonnectNow.WebAPI/Global.asax.cs
@@ -25,5 +25,20 @@ namespace KonnectNow.WebAPI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             AutoMapperConfig.Configure();
         }
+
+
+        private void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                //These headers are handling the "pre-flight" OPTIONS call sent by the browser
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST,PUT,DELETE");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, AccessToken");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
+
     }
 }
